f1ca79710eabb66059e22d441dee129d4fff7853
Marcin Kovalevskij <averageismarcin@gmail.com>
2024-04-16 17:29:21 +0200

    Update FSO_simulation_v2.py

--- FSO_simulation_v2.py
+++ FSO_simulation_v2.py
@@ -1,6 +1,7 @@
 from src import opticalpropagation
 from src import phasescreen
 from src import helper_functions as help_func
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,93 +10,72 @@ import threading
 import logging
 import json
 
-##############################################################
-#######################SIMULATION CONFIGRUATION###############
+#######################CONFIGURE LOGGING######################
+
+current_datetime = datetime.now()
 
 logger = logging.getLogger(__name__)
-
 logger.setLevel(logging.DEBUG)
-
 file_handler = logging.FileHandler('FSO_simulation_log.txt')
-
 file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
-
 logger.addHandler(file_handler)
-
 stream_handler = logging.StreamHandler()
-
 stream_handler.setFormatter(logging.Formatter('%(message)s'))
-
 logger.addHandler(stream_handler)
 
-#All values in a sim_config.json are in SI units
+#######################GET PARAMETERS########################
+
 with open('sim_config.json') as config_file:
     params = json.load(config_file)
 
-n_phase_screens = params['n_phase_screens'] 
+n_phase_screens = params['n_phase_screens']
+sim_res         = params['sim_res']
+sim_size        = params['sim_size']
+rx_alt          = params['rx_alt']
+rx_diameter     = params['rx_diameter']
+wvl             = params['wvl']         
+w0              = params['w0']
+power           = params['power']
+max_turb_alt    = params['max_turb_alt']
+min_turb_alt    = params['min_turb_alt']
+r0              = params['r0'] if len(params['r0']) != 0 else np.random.rand(n_phase_screens)*0.1
+r0              = np.full((n_phase_screens,), r0) 
+l0              = params['l0'] if len(params['l0']) != 0 else 1e-2
+l0              = np.full((n_phase_screens,), l0)  
+L0              = params['L0'] if len(params['L0']) != 0 else 50
+L0              = np.full((n_phase_screens,),L0)
+screen_alt      = np.linspace(min_turb_alt, min(max_turb_alt,rx_alt-min_turb_alt), n_phase_screens)
+screen_alt      = np.append(screen_alt, rx_alt)
 
-sim_res = params['sim_res']
-sim_size = params['sim_size']
-
-rx_alt = params['rx_alt']
-rx_diameter = params['rx_diameter']
-
-wvl = params['wvl']
-w0 = params['w0']
-power = params['power']
-
-max_turb_alt = params['max_turb_alt']
-min_turb_alt = params['min_turb_alt']
-
-wind_directions = np.full((n_phase_screens,), 0)
-params['wind_directions'] = wind_directions.tolist()
-
-wind_speeds = np.random.rand(n_phase_screens)*10
-params['wind_speeds'] = wind_speeds.tolist()
-
-r0 = params['r0'] if len(params['r0']) != 0 else np.random.rand()*0.1
-r0 = np.full((n_phase_screens,), r0)
-params['r0'] = r0.tolist() 
-
-l0 = params['l0'] if len(params['l0']) != 0 else 1e-2
-l0 = np.full((n_phase_screens,), l0)  
-params["l0"] = l0.tolist()
-
-L0 = params['L0'] if len(params['L0']) != 0 else 50
-L0 = np.full((n_phase_screens,),L0)
-params["L0"] = L0.tolist()
-
-screen_alt = np.linspace(min_turb_alt, min(max_turb_alt,rx_alt-min_turb_alt), n_phase_screens)
-screen_alt = np.append(screen_alt, rx_alt)
+params['r0']    = r0.tolist() 
+params["l0"]    = l0.tolist()
+params["L0"]    = L0.tolist()
 params["screen_alt"] = screen_alt.tolist()
+
+#######################SIMULATION CONFIGURATION###############
+
+logger.info(f"================= ============== =================")
+logger.info(f"================= SIMULATION START ===============")
+logger.info(f"================ {current_datetime.strftime('%Y-%m-%d %H:%M')} ================")
+logger.info(f"================= ============== =================")
 
 with open('sim_config.json', 'w') as config_file:
     json.dump(params, config_file, indent=4)
 
-
 for key, value in params.items():
     logger.info(f"{key}: {value}")
 
-beam_size = help_func.beam_size(rx_alt, w0, wvl)
-logger.info(f"Gaussian beam width =  {beam_size:.2f} m")
-
-if sim_size < 1.2*beam_size:
-    logger.warning(f"Small simulation size (screen) is detected. Set sim_size to ≥ {1.2*beam_size:.5f}")
-
-#######################SIMULATION CONFIGURATION###############
-##############################################################
-
-pxl_scale = sim_size/float(sim_res) #simulation pixel scale
+#Simulation pixel scale
+pxl_scale = sim_size/float(sim_res)
 
 #Discretize simulation grid
 nx, ny = np.meshgrid(np.arange(-sim_res/2,sim_res/2),
                      np.arange(-sim_res/2,sim_res/2))
-  
-delta = pxl_scale                   #simulation grid size dx
-xn = nx*delta                       #simulation grid scaled in x
-yn = ny*delta                       #simulation grid scaled in y
-r_sq = xn**2+yn**2                  
 
+#simulation grid scales in x,y
+xn      = nx*pxl_scale                       
+yn      = ny*pxl_scale                    
+r_sq    = xn**2+yn**2                  
 
 #Allocate phase screens
 phase_screens = np.zeros((sim_res,
@@ -140,8 +120,8 @@ for i in range(1, n_phase_screens+1):
     el_field *= np.exp(1j*phase_screens[:,:,i-1])
     el_field[:] = opticalpropagation.angularSpectrum(el_field,
                                                      wvl,
-                                                     delta,
-                                                     delta,
+                                                     pxl_scale,
+                                                     pxl_scale,
                                                      dz)
     
     progress_bar.update()
@@ -154,10 +134,12 @@ intensity = help_func.calc_intensity(el_field, power, w0)
 rx_intensity, rx_power = help_func.calc_RX_params(intensity,
                                                   sim_res,
                                                   rx_diameter,
-                                                  delta)
+                                                  pxl_scale)
 
-logger.info(f"Total power = {help_func.calc_tot_power(intensity, delta):.8f} W")
-logger.info(f"RX power =  {rx_power:.8f} W")
+logger.info(f"Total power = {help_func.calc_tot_power(intensity, pxl_scale):.10f} W")
+logger.info(f"RX power =  {rx_power:.10f} W")
+logger.info(f"RX power =  {10*np.log10(1000*rx_power/1):.10f} dBm")
+logger.info(f"Total Losses =  {10*np.log10(power/rx_power):.10f} dB")
 
 help_func.plot_intensity(rx_intensity, sim_size, rx_alt, "Aperture")
 help_func.plot_intensity(intensity, sim_size, rx_alt, "Total")
